75ca024a7c170e1c41212ff97fb9321d071d177c
75ks <kenta.s705@gmail.com>
2020-08-26 20:09:32 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,10 @@ class User < ApplicationRecord
     validates :gender_id
   end
 
-  validates :gender_id, numericality: { other_then: 1 }
+  validates :email, uniqueness: true
+
+  validates :gender_id, numericality: { other_than: 1, message: 'を選択してください' }
+
+  half_width_alphanumeric = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i
+  validates :password, presence: true, format: { with: half_width_alphanumeric, message: 'に半角英数字を使用してください' }
 end
