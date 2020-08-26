db91aec710818ed54cf29dca7ed8fff6ecb1bf68
75ks <kenta.s705@gmail.com>
2020-08-26 19:20:36 +0900

    userモデルのアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :gender
+  has_many               :posts
+
+  with_options presence: true do
+    validates :nickname
+    validates :gender_id
+  end
+
+  validates :gender_id, numericality: { other_then: 1 }
 end
