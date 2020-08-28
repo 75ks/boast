b630d11c851f8c625a3637d84037afe303b309e6
75ks <kenta.s705@gmail.com>
2020-08-28 17:12:14 +0900

    createアクションのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
-  resources :posts, only: [:new, :create, :show, :destroy, :edit, :update]
+  resources :posts, only: [:new, :create, :show, :destroy, :edit, :update] do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
