5e75e986c1e98165daaca0d1e9178a05b97121a3
75ks <kenta.s705@gmail.com>
2020-08-29 18:52:46 +0900

    複数画像投稿機能の実装

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -2,18 +2,39 @@ if (document.URL.match(/new/) || document.URL.match(/edit/)) {
   document.addEventListener("DOMContentLoaded", function(){
     const imageList = document.getElementById("image-list");
 
-    document.getElementById("post_images").addEventListener("change", function(e){
+    
+    const createImageHTML = function(blob) {
       
+      const imageElement = document.createElement("div");
+      imageElement.setAttribute("id", "image_element");
+      let imageElementNum = document.querySelectorAll("#image_element").length;
+      
+      const blobImage = document.createElement("img");
+      blobImage.setAttribute("src", blob);
+      
+      const inputHTML = document.createElement("input");
+      inputHTML.setAttribute("id", `post_image_${imageElementNum}`);
+      inputHTML.setAttribute("name", "post[images][]");
+      inputHTML.setAttribute("type", "file");
+      
+      
+      imageElement.appendChild(blobImage);
+      imageElement.appendChild(inputHTML);
+      imageList.appendChild(imageElement);
+      
+      inputHTML.addEventListener("change", function(e){
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+        
+        createImageHTML(blob);
+      });
+    }
+
+    document.getElementById("post_images").addEventListener("change", function(e){
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 
-      const imageElement = document.createElement("div");
-
-      const blobImage = document.createElement("img");
-      blobImage.setAttribute("src", blob);
-
-      imageElement.appendChild(blobImage);
-      imageList.appendChild(imageElement);
+      createImageHTML(blob);
     });
   });
 }
