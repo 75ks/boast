c3461c052cb50b10307379f3379d89041a40e9f8
75ks <kenta.s705@gmail.com>
2020-08-29 16:29:08 +0900

    commentモデルの単体テストコード

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'コメント' do
+    context 'コメントがうまくいくとき' do
+      it 'textが存在すれば登録できる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメントがうまくいかないとき' do
+      it 'textが空では登録できない' do
+        @comment.text = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("テキストを入力してください")
+      end
+      it 'textの文字数が50文字以下ではない場合登録できない' do
+        @comment.text = "テストです。テストです。テストです。テストです。テストです。テストです。テストです。テストです。テストです。"
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include("テキストは50文字以下にしてください")
+      end
+    end
+  end
 end
