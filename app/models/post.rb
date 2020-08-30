6354e6e89ee939290780531f9eb564339d88c10f
75ks <kenta.s705@gmail.com>
2020-08-30 12:37:11 +0900

    dependent: :destroyを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   has_many_attached :images
-  has_many         :comments
+  has_many         :comments, dependent: :destroy
   belongs_to       :user
 
   with_options presence: true do
