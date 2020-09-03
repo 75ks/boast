9a0fdef0d82422b03dde16879c7c90c442c784eb
75ks <kenta.s705@gmail.com>
2020-09-03 11:51:38 +0900

    create, destroyアクションを設定

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,35 @@
 class FavoritesController < ApplicationController
+  before_action :set_post, only: [:show, :create, :destroy]
+
   def show
 
   end
 
   def create
-
+    @favorite = Favorite.new(user_id: current_user.id, post_id: @post.id)
+    if @favorite.valid?
+      @favorite.save
+      redirect_to root_path
+    else
+      @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+      render template: "posts/index"
+    end
   end
 
   def destroy
+    @favorite = Favorite.find_by(user_id: current_user.id, post_id: @post.id)
+    if @favorite
+      @favorite.destroy
+      redirect_to root_path
+    else
+      @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(5)
+      render template: "posts/index"
+    end
+  end
 
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 end
