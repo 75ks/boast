2e6903e80e00ab803a67c24f9e51358519df9456
75ks <kenta.s705@gmail.com>
2020-09-03 16:40:04 +0900

    リダイレクトしないようにコントローラーを編集

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,24 +7,12 @@ class FavoritesController < ApplicationController
 
   def create
     @favorite = Favorite.new(user_id: current_user.id, post_id: @post.id)
-    if @favorite.valid?
-      @favorite.save
-      redirect_to root_path
-    else
-      @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(5)
-      render template: "posts/index"
-    end
+    @favorite.save
   end
 
   def destroy
     @favorite = Favorite.find_by(user_id: current_user.id, post_id: @post.id)
-    if @favorite
-      @favorite.destroy
-      redirect_to root_path
-    else
-      @posts = Post.includes(:user).order("created_at DESC").page(params[:page]).per(5)
-      render template: "posts/index"
-    end
+    @favorite.destroy
   end
 
   private
