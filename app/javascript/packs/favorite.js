32c9315ba38c9dee869235965dd04437bb483030
75ks <kenta.s705@gmail.com>
2020-09-06 11:54:08 +0900

    setIntervalで反応が悪くなる問題を修正

--- app/javascript/packs/favorite.js
+++ app/javascript/packs/favorite.js
@@ -1,25 +1,11 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   const hearts = document.querySelectorAll(".heart-count"); 
-//   hearts.forEach(function(heart) {
-
-//     heart.addEventListener("click", function() {
-//       const postId = this.getAttribute("data-id");
-//       const favoriteUser = document.getElementById(`favorites_user_${postId}`)
-//       console.log(favoriteUser);
-
-//       if (favoriteUser.getAttribute("style") == "display:block;") {
-//         favoriteUser.removeAttribute("style", "display:block;")
-//       } else {
-//         favoriteUser.setAttribute("style", "display:block;")
-//       }
-//     });
-//   });
-// });
-
 function check() {
   const hearts = document.querySelectorAll(".heart-count"); 
   hearts.forEach(function(heart) {
-    
+    if (heart.getAttribute("data-load") != null) {
+      return null;
+    }
+    heart.setAttribute("data-load", "true");
+
     heart.addEventListener("click", function() {
       const postId = this.getAttribute("data-id");
       const favoriteUser = document.getElementById(`favorites_user_${postId}`)
