64d05081ac52d47de5593709153d69579fe30070
75ks <kenta.s705@gmail.com>
2020-09-13 17:59:55 +0900

    スマホでイベント発火しないように設定

--- app/javascript/packs/favorite.js
+++ app/javascript/packs/favorite.js
@@ -1,22 +1,24 @@
-function check() {
-  const hearts = document.querySelectorAll(".heart-count"); 
-  hearts.forEach(function(heart) {
-    if (heart.getAttribute("data-load") != null) {
-      return null;
-    }
-    heart.setAttribute("data-load", "true");
-
-    heart.addEventListener("click", function() {
-      const postId = this.getAttribute("data-id");
-      const favoriteUser = document.getElementById(`favorites_user_${postId}`)
-      console.log(favoriteUser);
-      
-      if (favoriteUser.getAttribute("style") == "display:block;") {
-        favoriteUser.removeAttribute("style", "display:block;")
-      } else {
-        favoriteUser.setAttribute("style", "display:block;")
+if (window.matchMedia('(min-width:600px)').matches) {
+  function check() {
+    const hearts = document.querySelectorAll(".heart-count"); 
+    hearts.forEach(function(heart) {
+      if (heart.getAttribute("data-load") != null) {
+        return null;
       }
+      heart.setAttribute("data-load", "true");
+
+      heart.addEventListener("click", function() {
+        const postId = this.getAttribute("data-id");
+        const favoriteUser = document.getElementById(`favorites_user_${postId}`)
+        console.log(favoriteUser);
+        
+        if (favoriteUser.getAttribute("style") == "display:block;") {
+          favoriteUser.removeAttribute("style", "display:block;")
+        } else {
+          favoriteUser.setAttribute("style", "display:block;")
+        }
+      });
     });
-  });
+  }
+  setInterval(check, 1000);
 }
-setInterval(check, 1000);
